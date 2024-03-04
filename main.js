752d07bef5368b950ee28e74413edc2b2ced1b3d
03112006mari <118907487+03112006mari@users.noreply.github.com>
2024-03-04 09:30:18 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,5 +5,7 @@ for(let j=0;j<botoes.length;j++) { botoes[j].classList.remove(«ativo»);
 }
 botoes[i].classList.add(«ativo»);
    }
-                                  
+const textos = document.querySelectorAll(“.aba-conteudo”);
+ textos[i].classList.add(“ativo”);
+  textos[j].classList.remove(“ativo”);                                
 }
