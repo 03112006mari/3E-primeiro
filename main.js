48ba6cc23c54e9b405557d95f92e88833eba8654
03112006mari <118907487+03112006mari@users.noreply.github.com>
2024-03-04 09:41:26 -0300

    Update main.js

--- main.js
+++ main.js
@@ -9,3 +9,12 @@ const textos = document.querySelectorAll(“.aba-conteudo”);
  textos[i].classList.add(“ativo”);
   textos[j].classList.remove(“ativo”);                                
 }
+const contadores = document.querySelectorAll(“.contador”);
+const contadores = document.querySelectorAll(“.contador”);
+contadores[0]
+contadores[0].textContent = “Contagem regressiva”;
+const contadores = document.querySelectorAll(“.contador”);
+const tempoObjetivo1 = new Date(“2024-10-05”);
+const contadores = document.querySelectorAll(“.contador”);
+const tempoObjetivo1 = new Date(“2024-10-05”);
+contadores[0].textContent = tempoObjetivo1;
