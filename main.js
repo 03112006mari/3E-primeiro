67c808ed5be372bd06d328732751d4023aeba874
03112006mari <118907487+03112006mari@users.noreply.github.com>
2024-03-18 08:45:21 -0300

    Update main.js

--- main.js
+++ main.js
@@ -10,11 +10,23 @@ const textos = document.querySelectorAll(“.aba-conteudo”);
   textos[j].classList.remove(“ativo”);                                
 }
 const contadores = document.querySelectorAll(“.contador”);
-const contadores = document.querySelectorAll(“.contador”);
-contadores[0]
 contadores[0].textContent = “Contagem regressiva”;
 const contadores = document.querySelectorAll(“.contador”);
 const tempoObjetivo1 = new Date(“2024-10-05”);
-const contadores = document.querySelectorAll(“.contador”);
-const tempoObjetivo1 = new Date(“2024-10-05”);
-contadores[0].textContent = tempoObjetivo1;
+let tempoAtual = new Date();
+contadores[0].textContent = tempoObjetivo1-tempoAtual;
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+function calculaTempo(tempoObjetivo){
+let tempoAtual = new Date();
+   let tempoAtual = new Date();
+let tempoFinal = tempoObjetivo - tempoAtual;
+let segundos = tempoFinal / 1000;
+let minutos = Math.floor(segundos / 60);
+   let horas = Math.floor(minutos / 60);
+let dias = Math.floor(horas / 24);
+segundos %= 60;
+minutos %= 60;
+   horas %= 24;
+return dias + “ dias “ + horas + “ horas “ + minutos + “
+minutos “ + segundos + “ segundos”;
+}
